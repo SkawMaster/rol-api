f4b48e1a45c0142dd211eca4068ac3cc13509e52
cmateosl <mateos.c19@gmail.com>
2017-02-21 00:16:30 +0100

    Add javadoc.

--- src/main/java/es/esky/rol/security/CurrentUserDetails.java
+++ src/main/java/es/esky/rol/security/CurrentUserDetails.java
@@ -7,44 +7,72 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+/**
+ * Stores information about the current user logged.
+ */
 class CurrentUserDetails implements UserDetails {
 
     private User userInfo;
 
-    CurrentUserDetails(User userInfo) {
+    /**
+     * Construct a new CurrentUserDetails.
+     * @param userInfo User logged information.
+     */
+    public CurrentUserDetails(User userInfo) {
         this.userInfo = userInfo;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getPassword() {
         return userInfo.getPassword();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getUsername() {
         return userInfo.getUsername();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isAccountNonExpired() {
         return Boolean.TRUE;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isAccountNonLocked() {
         return Boolean.TRUE;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return Boolean.TRUE;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isEnabled() {
         return Boolean.TRUE;
