ba903effa23dd581148b5bbeb3de05bc0166c1a6
cmateosl <mateos.c19@gmail.com>
2017-02-21 00:04:52 +0100

    Add javadoc.

--- src/main/java/es/esky/rol/arch/domain/ApiError.java
+++ src/main/java/es/esky/rol/arch/domain/ApiError.java
@@ -1,22 +1,44 @@
 package es.esky.rol.arch.domain;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+
 import java.io.Serializable;
 
+/**
+ * Store an api error response.
+ */
 public class ApiError implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String code;
-    private String message;
+    private final String code;
+    private final String message;
 
-    public ApiError(String code, String message) {
+    /**
+     * Construct an api error.
+     * @param code    Unique error identification.
+     * @param message Message error.
+     */
+    public ApiError(final String code, final String message) {
         this.code = code;
         this.message = message;
     }
 
+    /**
+     * Get code error identification.
+     *
+     * @return Error identification.
+     */
+    @JsonGetter
     public String getCode() {
         return code;
     }
 
+    /**
+     * Get message error.
+     *
+     * @return Message error.
+     */
+    @JsonGetter
     public String getMessage() {
         return message;
     }
