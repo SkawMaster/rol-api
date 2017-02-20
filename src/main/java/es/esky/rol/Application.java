da9006798443381a797c3f68d4a0c2e84250ac35
cmateosl <mateos.c19@gmail.com>
2017-02-20 23:51:32 +0100

    Add javadoc.

--- src/main/java/es/esky/rol/Application.java
+++ src/main/java/es/esky/rol/Application.java
@@ -2,20 +2,30 @@ package es.esky.rol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * Api entry point class.
+ */
 @SpringBootApplication
 public class Application {
 
     private static final int ENCODER_STRENGTH = 10;
 
+    /**
+     * Entry point.
+     * @param args Application call arguments.
+     */
     public static void main(String... args) {
         SpringApplication.run(Application.class);
     }
 
+    /**
+     * Configured password encoder bean.
+     * @return A configured {@link BCryptPasswordEncoder}.
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(ENCODER_STRENGTH);
