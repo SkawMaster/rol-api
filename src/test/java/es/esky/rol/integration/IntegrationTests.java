136e45c6564943e166e7a91dc9276d01028d4ab6
Cristian Mateos López <mateos.c19@gmail.com>
2017-02-23 22:28:20 +0100

    Add integration tests

---
+++ src/test/java/es/esky/rol/integration/IntegrationTests.java
@@ -0,0 +1,16 @@
+package es.esky.rol.integration;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Cucumber integration tests loader.
+ *
+ * @author Cristian Mateos López
+ * @since 1.0.0
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", format = "pretty")
+public class IntegrationTests {
+}
