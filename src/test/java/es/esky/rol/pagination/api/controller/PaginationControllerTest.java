14d23f0012ba1460798e5cf676753f086d5896ad
Cristian Mateos López <mateos.c19@gmail.com>
2017-04-12 00:01:06 +0200

    Improve test coverage.

--- src/test/java/es/esky/rol/pagination/api/controller/PaginationControllerTest.java
+++ src/test/java/es/esky/rol/pagination/api/controller/PaginationControllerTest.java
@@ -16,8 +16,8 @@
 
 package es.esky.rol.pagination.api.controller;
 
-import java.lang.reflect.Method;
-
+import es.esky.rol.http.ApiHttpHeaders;
+import es.esky.rol.pagination.service.PaginationService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,80 +30,91 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 
-import es.esky.rol.http.ApiHttpHeaders;
-import es.esky.rol.pagination.service.PaginationService;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
-import static org.junit.Assert.assertThat;
+import java.lang.reflect.Method;
+
+import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasKey;
-import static org.hamcrest.Matchers.hasEntry;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PaginationControllerTest {
-    
+
     private Method pageableMethod;
     private Method nonPageableMethod;
-    
+
     @InjectMocks
     private PaginationController paginationController;
-    
+
     @Mock
     private PaginationService paginationService;
-    
+
     @Before
     public void setup() throws NoSuchMethodException {
         pageableMethod = SampleController.class.getMethod("pageableMethod");
         nonPageableMethod = SampleController.class.getMethod("nonPageableMethod");
     }
-    
+
     @Test
     public void beforeBodyWriteInternal_WriteHeaderPaginationData() {
+        final long expectedTotalCount = 11L;
+        final String expectedLink = "dummy";
+
         Page<?> samplePage = mock(Page.class);
         ServerHttpResponse response = mock(ServerHttpResponse.class);
-        
+        ServerHttpRequest request = mock(ServerHttpRequest.class);
+
         MappingJacksonValue value = new MappingJacksonValue(samplePage);
         HttpHeaders headers = new HttpHeaders();
-        
-        when(samplePage.getTotalElements()).thenReturn(11L);
+
+        when(samplePage.getTotalElements()).thenReturn(expectedTotalCount);
+        when(paginationService.buildHttpHeaderLinks(request, samplePage)).thenReturn(expectedLink);
         when(response.getHeaders()).thenReturn(headers);
-        
-        paginationController.beforeBodyWriteInternal(value, null, null, null, response);
-        
+
+        paginationController.beforeBodyWriteInternal(value, null, null, request, response);
+
         assertThat(headers, hasKey(ApiHttpHeaders.LINK));
-        assertThat(headers, hasEntry(ApiHttpHeaders.TOTAL_COUNT, "11"));
+        assertThat(headers, hasKey(ApiHttpHeaders.TOTAL_COUNT));
+        assertThat(headers.get(ApiHttpHeaders.LINK), equalTo(singletonList(expectedLink)));
+        assertThat(headers.get(ApiHttpHeaders.TOTAL_COUNT), equalTo(singletonList(String.valueOf(expectedTotalCount))));
     }
 
     @Test
     public void supports_PageReturnTypeAndJacksonConverter_ReturnTrue() {
         MethodParameter returnType = new MethodParameter(pageableMethod, -1, 0);
         boolean support = paginationController.supports(returnType, AbstractJackson2HttpMessageConverter.class);
-        
+
         assertThat(support, equalTo(Boolean.TRUE));
     }
-    
+
     @Test
     public void supports_NonPageReturnTypeAndJacksonConverter_ReturnFalse() {
         MethodParameter returnType = new MethodParameter(nonPageableMethod, -1, 0);
         boolean support = paginationController.supports(returnType, AbstractJackson2HttpMessageConverter.class);
-        
+
         assertThat(support, equalTo(Boolean.FALSE));
     }
-    
+
     @Test
     public void supports_PageReturnTypeAndNonJacksonConverter_ReturnFalse() {
         MethodParameter returnType = new MethodParameter(pageableMethod, -1, 0);
         boolean support = paginationController.supports(returnType, NonJacksonConverter.class);
-        
+
         assertThat(support, equalTo(Boolean.FALSE));
     }
-    
-    static interface SampleController {
+
+    @SuppressWarnings("unused")
+    interface SampleController {
         Page<Object> pageableMethod();
+
         Object nonPageableMethod();
     }
-    
-    static interface NonJacksonConverter extends HttpMessageConverter<Object> {}
+
+    interface NonJacksonConverter extends HttpMessageConverter<Object> {
+    }
 }
